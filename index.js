25741b1a7eb738cc23b465b0359c9c6b9aa41cfd
Your Name <your_email@example.com>
2024-03-11 17:11:58 +0530

    blog

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const blogRoute = require("./routes/blog")
 const cookieParse = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
+const Blog = require("./models/blog");
 
 const app = express();
 const PORT = 4001;
 
+
 mongoose.connect("mongodb://127.0.0.1:27017/blogify")
 .then(()=>console.log("MongoDB Connected"))
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"))
@@ -20,9 +23,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParse());
 app.use(checkForAuthenticationCookie("token"));
 
-app.get("/", (req, res)=>{
+app.get("/", async (req, res)=>{
+  const allBlog = await Blog.find({});
       res.render("home", {
         user: req.user,
+        blogs:allBlog,
     });
 });
 
